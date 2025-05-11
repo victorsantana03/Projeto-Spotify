de91d129e24e58eb0baa9522d990308334825411
victorsantana03 <viclucigei.123@gmail.com>
2025-05-10 22:08:24 -0300

    ajuste no process env

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import "dotenv/config";
 
-const NODE_ENV = process.env.NODE_ENV;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const NODE_ENV = process.env.NODE_ENV;
+const URL = "https://projeto-spotify-hyoi.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
